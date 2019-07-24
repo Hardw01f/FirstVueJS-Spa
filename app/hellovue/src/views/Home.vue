0d282eae54f023e4994531a10d0fc578e3be1039
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2019-07-24 17:46:05 +0900

    Add todo list but remain a warning

--- app/hellovue/src/views/Home.vue
+++ app/hellovue/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" width="15%" height="15%">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
