ecd3a4d37d07d9443d3bf69cd75dd078bc5a54d5
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2019-08-12 14:36:48 +0900

    Add components/Signin.vue

---
+++ app/hellovue/src/components/Signin.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class='signin'>
+		  <h1>test<h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods:{
+  },
+}
+
+
+
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+#time {
+		font-size: 600%;
+}
+
+#todayweek {
+        font-size: 400%;
+}
+
+#nextdayweek {
+		font-size: 200%;
+}
+
+#garbage {
+		font-size: 100%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
