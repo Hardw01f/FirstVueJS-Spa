24ee94dc72561a262b87d4e85b58207a75a173a8
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2019-08-20 20:45:26 +0900

    comment test() function and fixing methods

--- app/hellovue/src/App.vue
+++ app/hellovue/src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link v-if="userStatus" to="/">Home</router-link> |
       <router-link to="/shopping">Shopping-Lists</router-link> |
       <router-link to="/signin">Signin</router-link> |
       <router-link to="/signup">Signup</router-link> |
       <!-- <Signout @click="signOut">Signout</Signout> -->
-      <router-link to="/signOut">Signout</router-link>
+      <router-link to="/signOut">Signout</router-link> |
 
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+
+/*
+function test() {
+		firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log('test login')
+                return true
+            } else {
+                console.log('test logout')
+                return false
+			}
+		})
+}*/
+
+
+export default {
+    name: "statusCheck",
+    data () {
+            return {
+                userStatus : this.statusCheck
+            }
+    },
+    methods: {
+        statusCheck: function () {
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    console.log('login')
+                    return true
+                } else {
+                    console.log('logout')
+                    return false 
+                }
+            });
+		}
+    }
+}
+
+</script>
 
 
 <style>
